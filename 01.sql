acacb3ad13e967595e062b2036fd4a569ffd1f49
Han1234 <a@a.com>
2023-01-17 14:03:52 +0900

    'a'

--- 01.sql
+++ 01.sql
@@ -4,19 +4,22 @@ from departments;
 select department_id, location_id
 from departments;
 
+/*language 이기 때문에 ;로 마무리한다.*/
 select location_id, department_id
-from departments;/*language 이기 때문에 ;로 마무리한다.*/
+from departments;
 
-desc departments /*command 이기 때문에 ;로 마무리하지 않는다.*/
+/*command 이기 때문에 ;로 마무리하지 않는다.*/
+desc departments
 
+--select 절에서, expression의 사용.
 select last_name, salary, salary + 300
 from employees;
 
 -- 과제: 사원들의 월급, 연봉을 조회하라.
-
 select salary, salary * 12
 from employees;
 
+--sql에서도 연산자 우선순위가 존재한다.
 select last_name, salary, 12 * salary + 100
 from employees;
 
@@ -46,14 +49,17 @@ select employee_id "Emp #", last_name "Name", job_id "Job", hire_date "Hire Date
 from employees;
 
 /*붙이기 연산자 ||를 이용하여 column을 하나로 만든다.*/
+--필드값들이 띄어쓰기 없이 붙는것을 확인.
 select last_name || job_id
 from employees;
 
 /*is와 같은 상수를 쓸수도 있다.*/
+--필드값 사이에 ' is ' 삽입이 되는것을 확인.
 select last_name || ' is ' || job_id
 from employees;
 
 /*column 명(last_name || ' is ' || job_id)을 별명 employee로 표현했다.*/
+--필드 label은 별명으로 바뀌고, 필드값들은 is로 연결되는것을 확인.
 select last_name || ' is ' || job_id employee
 from employees;
 
@@ -61,6 +67,7 @@ from employees;
 select last_name || null
 from employees;
 
+/*commission_pct field의 null값들이 parsing 되는것을 확인하자.*/
 select last_name || commission_pct
 from employees;
 
@@ -75,5 +82,5 @@ select last_name || (salary * 12)
 from employees;
 
 -- 과제: 사원들의 '이름, 직업'(Emp and Title)을 조회하라.
-select last_name ||', '|| job_id "Emp and Title"
+select last_name || ', ' || job_id "Emp and Title"
 from employees;
--- 02.sql
+++ 02.sql
@@ -1,1 +1,101 @@
--- where
+-- where 의 활용 
+/*같다를 표현하는 연산자 = */
+select employee_id, last_name, department_id
+from employees
+where department_id = 50;
+
+-- 과제: 176번 사원의 사번, 이름, 부서번호를 조회하라.
+select employee_id, last_name, department_id
+from employees
+where employee_id = 176;
+
+/*data에서는 대소문자가 구별된다.*/
+select employee_id, last_name, department_id
+from employees
+where last_name = 'Whalen';
+
+/*date type의 검색은 char type을 이용한다.*/
+select employee_id, last_name, hire_date
+from employees
+where hire_date = '2008/02/06';
+
+select last_name, salary
+from employees
+where salary <= 3000;
+
+-- 과제: 월 $12,000 이상 버는 사원들의 이름, 월급을 조회하라.
+select last_name, salary
+from employees
+where salary >= 12000;
+
+/*다르다 연산자 != */
+select last_name, job_id
+from employees
+where job_id != 'IT_PROG';
+
+/*범위를 표현, 여기서는 이상 이하*/
+select last_name, salary
+from employees
+where salary between 2500 and 3500;
+
+/*K에서 S값 까지의 character를 출력*/
+select last_name
+from employees
+where last_name between 'King' and 'Smith';
+
+-- 과제: 'King' 사원의 first_name, last_name, 직업, 월급을 조회하라.
+select first_name, last_name, job_id, salary
+from employees
+where last_name = 'King';
+
+select last_name, hire_date
+from employees
+where hire_date between '2002/01/01' and '2002/12/31';
+
+/*n번 사원을 상사로 두는 사원을 출력.*/
+select employee_id, last_name, manager_id
+from employees
+where manager_id in (100, 101, 201);
+
+-- 조건 n개를 나열.
+select employee_id, last_name, manager_id
+from employees
+where manager_id = 100 or
+    manager_id = 101 or
+    manager_id = 201;
+
+-- 조건이 포함된 항목을 출력.
+select employee_id, last_name
+from employees
+where last_name in ('Hartstein', 'Vargas');
+
+select last_name, hire_date
+from employees
+where hire_date in ('2003/06/17', '2005/09/21');
+
+-- 비슷하다 '같다부분 + 와일드카드(%)'
+-- 첫 글자가 S인 사원명을 호출한다.
+select last_name
+from employees
+where last_name like 'S%';
+
+-- 끝나는 글자가 r인 사원명을 호출한다.
+select last_name
+from employees
+where last_name like '%r';
+
+-- 과제: 이름에 s 가 포함된 사원들의 이름을 조회하라.
+select last_name
+from employees
+where last_name like 'S%' or 
+    last_name like '%s'; /*틀림. 요구사항의 대소문자와 위치를 구별하라.*/
+
+--정답 
+select last_name
+from employees
+where last_name like '%s%';
+
+-- 과제: 2005년에 입사한 사원들의 이름, 입사일을 조회하라.
+select last_name, hire_date
+from employees
+where hire_date like '2005%';
