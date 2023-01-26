c7436c09e993c29588134168cd38b036804c018b
Han1234 <a@a.com>
2023-01-26 12:36:35 +0900

    'a'

---
+++ 11.sql
@@ -0,0 +1,177 @@
+/* view 객체 */
+create view empvu80 as
+    select employee_id, last_name, department_id
+    from employees
+    where department_id = 80;
+    
+desc empvu80
+
+select *
+from empvu80;
+
+select *
+from (select employee_id, last_name, department_id
+        from employees
+        where department_id = 80);
+        
+--view 수정하기
+create or replace view empvu80 as
+    select employee_id, job_id
+    from employees
+    where department_id = 80;
+
+desc empvu80
+
+-- 과제: 50번 부서원들의 사번, 이름, 부서번호로 구성된 DEPT50 view를 만들어라.
+--       view 구조는 EMPNO, EMPLOYEE, DEPTNO 이다.
+/*
+create or replace view dept50(empno, employee, deptno) as
+    select employee_id, last_name, department_id
+    from employees
+    where department_id = 50;
+*/
+create view dept50 as
+    select employee_id empno, last_name employee, department_id deptno
+    from employees
+    where department_id = 50;
+    
+desc dept50
+
+select *
+from dept50;
+------------------------------------------------------------
+create or replace view dept50(empno, employee, deptno) as
+    select employee_id, last_name, department_id
+    from employees
+    where department_id = 50
+    with check option constraint dept50_ch;
+    
+create table teams as
+    select department_id team_id, department_name team_name
+    from hr.departments;
+    
+create view team50 as
+    select *
+    from teams
+    where team_id = 50;
+
+select * from team50;
+
+--teams table에는 27개 부서 존재
+select count(*) from teams;
+
+--view를 통한 insert
+insert into team50
+values(300, 'Marketing');
+--위 코드가 teams table에 insert되어 부서가 28개로 증가.
+select count(*) from teams;
+
+--check option으로 50번 부서 설정.
+create or replace view team50 as
+    select *
+    from teams
+    where team_id = 50
+    with check option;
+    
+insert into team50 values(50, 'IT Support');
+--insert가 성공하여 부서가 29개로 증가.
+select count(*) from teams;
+-- with check option에 걸려 insert 불가능.
+insert into team50 values(301, 'IT Support');
+
+-- insert update delete 거부 설정.
+create or replace view empvu10(employee_num, employee_name, job_title) as
+    select employee_id, last_name, job_id
+    from employees
+    where department_id = 10
+    with read only;
+    
+insert into empvu10 values(501, 'abel', 'Sales');
+select * from empvu10;
+
+------------------------------------------------------------
+/* sequence 객체 */
+drop sequence team_teamid_seq;
+
+create sequence team_teamid_seq;
+
+select team_teamid_seq.nextval from dual; /*새로운값*/
+select team_teamid_seq.nextval from dual; /*새로운값*/
+select team_teamid_seq.currval from dual; /*현재값*/
+
+insert into teams
+values(team_teamid_seq.nextval, 'Marketing');
+
+select * from teams
+where team_id < 5;
+
+--sequence 복잡화
+create sequence x_xid_seq
+    start with 10  /*시작값*/
+    increment by 5 /*증가치*/
+    maxvalue 20    /*최고치*/
+    nocache        /*미리 준비 안하겠다를 설정*/
+    nocycle;       /*최고치 도달 후 순환 안한다.*/
+    
+select x_xid_seq.nextval from dual; /*20에서 max value 초과하면 오류뜸.*/
+
+-- 과제: DEPT 테이블의 DEPARTMENT_ID 칼럼의 field value로 쓸 sequence를 만들어라.
+--       sequence는 400이상 1000이하로 생성한다. 10씩 증가한다.
+drop sequence dept_deptid_seq;
+
+create sequence dept_deptid_seq
+    start with 400
+    increment by 10
+    maxvalue 1000
+    nocache
+    nocycle;
+
+select dept_deptid_seq.nextval from dual;
+
+-- 과제: 위 sequence를 이용해서, DEPT 테이블에서 Education 부서를 insert하라.
+insert into dept(department_id, department_name)
+values(dept_deptid_seq.nextval, 'Education');
+
+select * from dept;
+
+commit;
+
+------------------------------------------------------------
+/* index 객체 */
+drop index emp_lastname_idx;
+
+create index emp_lastname_idx
+on employees(last_name);
+--index에 해당하는 DB내에서 유일한 값 "rowid"
+select last_name, rowid
+from employees;
+
+select last_name
+from employees
+where rowid = 'AAAEAbAAEAAAADNABe';
+
+select index_name, index_type, table_owner, table_name
+from user_indexes;
+
+-- 과제: DEPT 테이블의 DEPARTMENT_NAME 에 대해 index를 만들어라.
+create index dept_deptname_idx
+on dept(department_name);
+
+select department_name, rowid
+from dept;
+
+------------------------------------------------------------
+/* synonym 객체 */
+drop synonym team;
+drop synonym emps;
+
+create synonym team
+for departments;
+
+select * from team;
+
+-- 과제: EMPLOYEES 테이블에 EMPS synonym 을 만들어라.
+create synonym emps
+for employees;
+
+select * from emps;
--- you.sql
+++ you.sql
@@ -37,7 +37,8 @@ from user_constraints;
 --data insert
 insert into depts values(100, 'Development');
 insert into emps values(500, 'musk', 'musk@gmail.com', 5000, 100);
---/*제약조건 오류 구경해보기.*/insert into emps values(501, 'a', 'a@gmail.com', 2000, 100);
+/*제약조건 오류 구경해보기.*/
+--insert into emps values(501, 'a', 'a@gmail.com', 2000, 100);
 
 /*table과 제약조건 모두 삭제해줘야 한다.*/
 drop table emps cascade constraints;
@@ -47,3 +48,120 @@ from user_constraints;
 
 select *
 from depts;
+
+------------------------------------------------------------
+/*system user로 실행하는 부분*/
+--system user가 hr.departments의 모든 권한을 you에서 사용가능하게 grant 하다.
+grant all on hr.departments to you;
+
+------------------------------------------------------------
+/*you user로 실행하는 부분*/
+drop table employees cascade constraints;
+create table employees(
+employee_id number(6) constraint emp_empid_pk primary key,
+first_name varchar2(20),
+last_name varchar2(25) constraint emp_lastname_nn not null,
+email varchar2(25) constraint emp_email_nn not null
+                    constraint emp_email_uk unique,
+phone_number varchar2(20),
+hire_date date constraint emp_hiredate_nn not null,
+job_id varchar2(10) constraint emp_jobid_nn not null,
+salary number(8) constraint emp_salary_ck check(salary > 0),
+commission_pct number(2, 2),
+manager_id number(6) constraint emp_managerid_fk 
+                            references employees(employee_id),
+department_id number(4) constraint emp_dept_fk 
+                            references hr.departments(department_id));
+------------------------------------------------------------
+/*부모와 자식 table 연습*/
+create table gu (
+gu_id number(3) primary key,
+gu_name char(9) not null);
+
+create table dong (
+dong_id number(4) primary key,
+dong_name varchar2(12) not null,
+/*gu와 dong 사이의 relationship을 references로 표현*/
+/*on delete cascade 는 부모 테이블이 없어지면 같이 사라지도록 한 것.*/
+gu_id number(3) references gu(gu_id) on delete cascade);
+
+create table dong2 (
+dong_id number(4) primary key,
+dong_name varchar2(12) not null,
+gu_id number(3) references gu(gu_id) on delete set null);
+
+/*부모, 자식 table에 data insert.*/
+insert into gu values(100, '강남구');
+insert into gu values(200, '노원구');
+
+insert into dong values(5000, '압구정동', null);
+insert into dong values(5001, '삼성동', 100);
+insert into dong values(5002, '역삼동', 100);
+insert into dong values(6001, '상계동', 200);
+insert into dong values(6002, '중계동', 200);
+
+insert into dong2
+select * from dong;
+
+delete gu
+where gu_id = 100;
+
+select * from dong;     /*delete cascade의 효과*/
+select * from dong2;    /*delete set null의 효과*/
+
+commit;
+------------------------------------------------------------
+create table a (
+aid number(1) constraint a_aid_pk primary key);
+
+create table b (
+bid number(2),
+aid number(1),
+constraint b_aid_fk foreign key(aid) references a(aid));
+
+insert into a values(1);
+insert into b values(31, 1);
+insert into b values(32, 9);/*제약조건 해제 후 insert 가능.*/
+
+/*table b의 b_aid_fk의 제약조건 해제.*/
+alter table b disable constraint b_aid_fk;
+
+/*이미 table b에 부모테이블에 없는 9가 존재하기에 제약 재설정 불가.*/
+alter table b enable constraint b_aid_fk;
+
+/*강제 제약 재설정.*/
+alter table b enable novalidate constraint b_aid_fik;
+
+/*제약조건 부활로 부모에 없는 key(8)로 insert 불가능.*/
+insert into b values(33, 8);
+
+/*hr.departments에서 sub_departments로 column 복사해서 table생성하기.*/
+create table sub_departments as
+    select department_id dept_id, department_name dept_name
+    from hr.departments;
+--잘 복사됨을 확인.
+select * from sub_departments;
+
+--table 구조 변경법.
+create table users(
+user_id number(3));
+desc users
+--column 추가.
+alter table users add(user_name varchar2(10));
+desc users
+--column type update.
+alter table users modify(user_name number(7));
+desc users
+--column delete
+alter table users drop column user_name;
+desc users
+
+insert into users values(1);
+
+alter table users read only;
+insert into users values(2);
+
+alter table users read write;
+insert into users values(2);
+
+commit;
